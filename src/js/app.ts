077ff9d42b6c7ba0df8e1e9b1548bb07157ee6ad
harutete <ai.takeuchi.10@gmail.com>
2020-01-15 18:03:39 +0900

    修正

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,8 +1,24 @@
 import '../sass/style.scss'
 
 class ramenMaster {
+  btnStart: any
   constructor() {
+    this.btnStart = document.querySelector('.js-btn-start')
   }
-  init () {
+  init() {
+    document.body.setAttribute('data-script-enabled', 'true')
+    this.btnStart.addEventListener('click', () => {
+      this.indicatePlayingScreen()
+    })
+  }
+  indicatePlayingScreen() {
+    const playingScreen: HTMLElement = document.querySelector('.js-content-playing-screen') as HTMLElement
+    this.btnStart.style.display = 'none'
+    playingScreen.style.display = 'block'
+  }
+  setOrder() {
+
   }
 }
+
+new ramenMaster().init()
