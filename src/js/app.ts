f7c36b3299a81ec2742053323ffa849023101318
harutete <ai.takeuchi.10@gmail.com>
2020-01-19 15:11:56 +0900

    判定のメソッドを追加

--- src/js/app.ts
+++ src/js/app.ts
@@ -4,25 +4,28 @@ import ingredientsData = require('../json/ingredients.json')
 import shuffleArray from './shuffle'
 
 class ramenMaster {
-  btnStart: any
+  startBtn: any
+  selectedMenu: any | null
+
   constructor() {
-    this.btnStart = document.querySelector('.js-btn-start')
+    this.startBtn = document.querySelector('.js-btn-start')
+    this.selectedMenu = null
   }
   init() {
     document.body.setAttribute('data-script-enabled', 'true')
-    this.btnStart.addEventListener('click', () => {
+    this.startBtn.addEventListener('click', () => {
       this.indicatePlayingScreen()
-      this.setOrder()
+      this.setSelectedMenuContent()
     })
   }
   indicatePlayingScreen() {
     const playingScreen = document.querySelector('.js-content-playing-screen') as HTMLElement
-    this.btnStart.style.display = 'none'
+    this.startBtn.style.display = 'none'
     playingScreen.style.display = 'block'
   }
-  setOrder() {
+  setSelectedMenuContent() {
     const menu = menuData
-    const selectedMenu = menu[Math.floor(Math.random() * menu.length)]
+    this.selectedMenu = menu[Math.floor(Math.random() * menu.length)]
     const orderText = document.querySelector('.js-text-order')
     const customerImage = document.querySelector('.js-img-customer')
     const customer = [
@@ -35,9 +38,9 @@ class ramenMaster {
     const orderImg = document.querySelector('.js-img-order')
 
     customerImage.setAttribute('src', customer[customerIndex])
-    orderText.textContent = selectedMenu.description
-    orderImg.setAttribute('src', selectedMenu.image)
-    orderImg.setAttribute('alt', selectedMenu.description)
+    orderText.textContent = this.selectedMenu.description
+    orderImg.setAttribute('src', this.selectedMenu.image)
+    orderImg.setAttribute('alt', this.selectedMenu.description)
 
     this.setIngredientsPanel()
   }
@@ -49,17 +52,53 @@ class ramenMaster {
     let panelHtml = ''
 
     shuffledingredients.forEach(item => {
-      panelHtml += `<div class="js-panel-ingredients panel-ingredients"><span class="panel-ingredients-inner"><img src="${item.image}"></span></div>`
+      panelHtml += `<div data-ingredients-id=${item.id} class="js-panel-ingredients panel-ingredients"><span class="panel-ingredients-inner"><img src="${item.image}" class="js-panel-ingredients-img"></span></div>`
     })
 
     outputElem.insertAdjacentHTML("afterbegin", panelHtml)
+    this.findPanelElem()
+  }
+  findPanelElem() {
+    const panels = document.querySelectorAll('.js-panel-ingredients')
+    const completeBtn = document.querySelector('.js-btn-complete')
+
+    panels.forEach(item => {
+      item.addEventListener('click', this.togglePanelClass)
+    })
+
+    completeBtn.addEventListener('click', () => {
+      this.checkIngredientsCombination()
+    })
   }
   // クリックされたら具のパネルの色を変えるクラスをつけるメソッド
-  togglePanelClass(elem: any) {
-    console.log(elem)
+  togglePanelClass(event: any) {
+    const clickedElemParent = event.target.closest('.js-panel-ingredients')
+
+    clickedElemParent.classList.toggle('is-active')
   }
   // OKボタンを押して組み合わせが正解かどうか判定するメソッド
   checkIngredientsCombination() {
+    const clickedPanels = document.querySelectorAll('.js-panel-ingredients.is-active')
+    const IngredientsIdArray: number[] = [...clickedPanels].map((elem) => {
+      return parseInt(elem.getAttribute('data-ingredients-id'), 10)
+    }).sort((a: number, b: number): number => {
+      return a - b
+    })
+    const correctIngredientsIdArray: number[] = this.selectedMenu.ingredients.map((item:any) => {
+      return parseInt(item.id, 10)
+    }).sort((a: number, b: number): number => {
+      return a - b
+    })
+    const correctIndgredientsLength = correctIngredientsIdArray.length
+    const differenceArray = IngredientsIdArray.filter((item: number, index: number) => {
+      return item === correctIngredientsIdArray[index]
+    })
+
+    if (differenceArray.length === correctIndgredientsLength) {
+      console.log('正解')
+    } else {
+      console.log('不正解')
+    }
   }
 }
 
