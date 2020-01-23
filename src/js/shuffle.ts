9f0094712673a00a2a02e226ab5457b1a68fba86
harutete <ai.takeuchi.10@gmail.com>
2020-01-23 20:04:34 +0900

    修正

--- src/js/shuffle.ts
+++ src/js/shuffle.ts
@@ -4,12 +4,12 @@ export default function shuffleArray(arr: { [key: string]: any }[]): { [key: str
   let i: number
 
   for (i = length - 1; i >= 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1))
-    const tmp = array[i]
+    const randomIndex: number = Math.floor(Math.random() * (i + 1)) as number
+    // const tmp = array[i]
 
-    array[i] = array[randomIndex]
-    array[randomIndex] = tmp
-    // [array[i], array[randomIndex]] = [array[randomIndex], array[i]]
+    // array[i] = array[randomIndex]
+    // array[randomIndex] = tmp
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]]
   }
 
     return array
