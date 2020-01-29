dc1e3357e0727ee9dbb70326b7f7e50badaec203
harutete <ai.takeuchi.10@gmail.com>
2020-01-29 22:07:27 +0900

    タイマー関数修正

--- src/js/Timer.ts
+++ src/js/Timer.ts
@@ -1,27 +1,42 @@
 class Timer {
   private readonly _limit: number
   private _wrap: HTMLElement
+  private timerId: null | any
   constructor(wrap: HTMLElement, ms: number) {
     this._wrap = wrap
     this._limit = ms
+    this.timerId = null
   }
-  countDownTimer(): void {
+  countdownTimer(): void {
     let timeLimitMs = this._limit
-    const timerId = () => {
+    this.timerId = (): void => {
       timeLimitMs = timeLimitMs - 1000
       const formatLimit = Math.floor(timeLimitMs / 1000)
       this._wrap.textContent = formatLimit.toString()
 
       setTimeout(() => {
-        timerId()
+        this.timerId()
       }, 1000)
     }
 
-    timerId()
+    if (timeLimitMs === 0) {
+      clearTimeout(this.timerId)
+    }
+
+    this.timerId()
   }
-  // 秒数をフォーマットする
   // タイマーを一時的に止める
-  // タイマーを再開させる
+  pauseCountdown(event: any, ms: number) {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        event()
+        resolve()
+      }, ms)
+    })
+    promise.then(() => {
+      clearTimeout(this.timerId)
+    })
+  }
 }
 
 export default Timer
