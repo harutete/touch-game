316fbbe84d4b16b6f05958203fa2013756201a07
harutete <ai.takeuchi.10@gmail.com>
2020-01-30 22:37:50 +0900

    タイマーの処理修正

--- src/js/Timer.ts
+++ src/js/Timer.ts
@@ -1,29 +1,26 @@
 class Timer {
   private readonly _limit: number
   private _wrap: HTMLElement
-  private timerId: null | any
+  private _timerId: null | any
+  private _count: number
   constructor(wrap: HTMLElement, ms: number) {
     this._wrap = wrap
     this._limit = ms
-    this.timerId = null
+    this._timerId = null
+    this._count = 0
   }
   public countdownTimer(): void {
-    let limitMs = this._limit
-    this.timerId = (): void => {
-      limitMs = limitMs - 1000
-      const limitSec = Math.floor(limitMs / 1000)
-      this._wrap.textContent = limitSec.toString()
+    let limitMs = this._limit - (1000 * this._count)
+    const limitSec = Math.floor(limitMs / 1000)
+    this._wrap.textContent = limitSec.toString()
+    this._count++
 
-      setTimeout(() => {
-        this.timerId()
-      }, 1000)
-    }
+    this._timerId = setTimeout(() => this.countdownTimer(), 1000)
 
     if (limitMs === 0) {
-      clearTimeout(this.timerId)
+      clearTimeout(this._timerId)
+      this._count = 0
     }
-
-    this.timerId()
   }
   // タイマーを一時的に止める
   public pauseCountdown(method: any, ms: number): void {
@@ -34,7 +31,7 @@ class Timer {
       }, ms)
     })
     promise.then(() => {
-      clearTimeout(this.timerId)
+      clearTimeout(this._timerId)
     })
   }
 }
