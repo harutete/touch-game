f824b59629e8f241e917ddecf8fd5cc7eb411d52
harutete <ai.takeuchi.10@gmail.com>
2020-01-31 14:17:24 +0900

    init関数追加

--- src/js/AddScore.ts
+++ src/js/AddScore.ts
@@ -1,7 +1,10 @@
 class AddScore {
-  private _totalScore: number
+  private _totalScore: number | null
   constructor() {
-    this._totalScore = 0
+    this._totalScore = null
+  }
+  public init(): number {
+    return this._totalScore = 0
   }
   public appendScore(score: number, count: number): number {
     return this._totalScore = score * count
