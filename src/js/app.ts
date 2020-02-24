1efb184b78a3e654d7a42299f15ec8cfa16df76b
harutete <ai.takeuchi.10@gmail.com>
2020-02-25 02:09:25 +0900

    制限時間を修正

--- src/js/app.ts
+++ src/js/app.ts
@@ -75,7 +75,7 @@ class TouchGame {
     const beforeScreen = document.querySelector('.js-content-before-screen') as HTMLElement
     const playingScreen = document.querySelector('.js-content-playing-screen') as HTMLElement
     const finishScreen = document.querySelector('.js-content-after-screen') as HTMLElement
-    const limit = 1000 * 3 // 3分
+    const limit = 1000 * 60 * 3 // 3分
     this._timer = new Timer(document.querySelector('.js-state-time'), limit, this.indicateFinishScreen.bind(this))
 
     this._startBtn.style.display = 'none'
